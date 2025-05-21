12655ca9e99f04387e3f8d90a7c76bf1158cbe89
s7star003 <s7star003@gmail.com>
2025-05-21 17:55:34 +0200

    完成TIKTOK ORDERLIST

--- tiktok_api/urls.py
+++ tiktok_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import get_order_list_view, get_order_detail_view
 
 urlpatterns = [
-    path('getorderlist', get_order_list_view),
-    path('getorderdetail', get_order_detail_view),
+    path('getOrderList', get_order_list_view),
+    path('getOrderDetail', get_order_detail_view),
 ]
